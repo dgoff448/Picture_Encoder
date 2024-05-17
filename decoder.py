7bec3422ff432da5c5fbf07eebc056e5ea058b65
David <77478567+dgoff448@users.noreply.github.com>
2024-05-17 12:39:55 -0400

    Add files via upload

---
+++ decoder.py
@@ -0,0 +1,40 @@
+from PIL import Image
+
+def get_length(pixel: tuple) -> int:
+    '''
+    converts the 3 digit base 256 integer to a base 10 value
+    '''
+    return int(pixel[0]) * (256**2) + int(pixel[1])* (256**1) + int(pixel[2])
+
+def int_to_char(num: int) -> str:
+    '''
+    converts an integer (between 0 and 127) to an ASCII character
+    '''
+    
+    # print(num)
+    binary = format(num, "08b")
+    char = chr(int(binary[1:], 2))
+    return char if char != "\x00" else ""
+
+def ints_to_str(int_list: list) -> str:
+    contents = ""
+    for i in int_list:
+        contents += int_to_char(i)
+    return contents
+
+with Image.open("encoded_picture.png") as img:
+    img_data = list(img.getdata())
+    payload_length = get_length(img_data.pop()) # last tuple stores length of the payload
+    print("Payload length:", payload_length)
+    ext_len = get_length(img_data.pop()) # second-last tuple stores length of the extension
+    print("Extension length:", ext_len)
+
+    img_data = [x for pixels in img_data for x in pixels[:3]]
+    ext = ints_to_str(img_data[payload_length:payload_length+ext_len])
+    print("Extension:", ext)
+
+    message = ints_to_str(img_data[:payload_length])
+    # print(message)
+
+with open("outputs/output." + ext, "w") as file:
+    file.write(message)
---
+++ encoder.py
@@ -0,0 +1,81 @@
+from PIL import Image
+import numpy as np
+import math
+
+class Payload:
+    def __init__(self, picture_fn, payload_fn):
+        self.picture_fn = picture_fn
+        self.payload_fn = payload_fn
+        self.payload_ext = payload_fn.split(".")[-1]
+        self.img = Image.open(picture_fn)
+        self.maxChars = (self.img.size[0] * self.img.size[1] * 3) - 6
+        self.payload = ""
+
+        with open(payload_fn, 'r', encoding="UTF-8") as f:
+            for line in f.readlines():
+                self.payload += line
+
+        self.payload_len = len(self.payload) # BEFORE EXTENSION ADDED
+        print(self.payload)
+        print(f"Payload Length: {len(self.payload)}")
+        print(f"Extension Length: {len(self.payload_ext)}")
+        self.payload = self.payload + self.payload_ext
+
+    # Checks if image can hold payload.
+    def canFit(self) -> bool:
+        return False if len(self.payload) > self.maxChars else True
+    
+    # Converts length int to an RGB tuple
+    def to_base_256(self, n):
+        result = []
+        while n > 0:
+            result.append(n % 256)
+            n = n // 256
+        if len(result) == 1:
+            result.append(0)
+            result.append(0)
+        elif len(result) == 2:
+            result.append(0)
+        result.reverse()  # because we need the most significant byte first
+        return tuple(result)
+    
+    # Encode payload into image array
+    def encode(self):
+        arr = np.array(self.img)
+        arr[-1][-1] = self.to_base_256(self.payload_len)     # last RGB value holds the length of the payload (read like a normal num)
+        arr[-1][-2] = self.to_base_256(len(self.payload_ext))    # second-to-last RGB value holds the length of the payload extension
+
+        print(arr[-1][-1], print(arr[-1][-2]))
+
+        pc = 0
+        for i, ar in enumerate(arr):
+            for j, a in enumerate(ar):
+                oldTup = list(a)
+                newArr = [0, 0, 0]
+                for k, rgb in enumerate(oldTup):
+                    if self.payload[pc] in ["‘", "’"]:
+                        newArr[k] = int(format(rgb, "08b")[:1] + format(ord("'"), "08b")[1:], 2)
+                    elif self.payload[pc] in ['“', '”']:
+                        newArr[k] = int(format(rgb, "08b")[:1] + format(ord('"'), "08b")[1:], 2)
+                    else:
+                        newArr[k] = int(format(rgb, "08b")[:1] + format(ord(self.payload[pc]), "08b")[1:], 2)
+                    pc += 1
+                    if pc > len(self.payload)-1:
+                        break
+                arr[i][j] = tuple(newArr)
+                if pc > len(self.payload)-1:
+                        break
+            if pc > len(self.payload)-1:
+                        break
+
+        img = Image.fromarray(arr)
+        img.save('encoded_picture.png', quality=100, subsampling=0)
+                    
+payload_file = input("Enter Payload Filename: ")
+picture_file = input("Enter Picture Filename: ")
+pl = Payload(f"images/{picture_file}", payload_file)
+print(pl.payload_ext)
+if not pl.canFit():
+    raise("Not enough space in image to encode payload.")
+
+pl.encode()
