49a001aadb893d51354d606a432b902930b1ddaa
Isaac Balardelle <isaac.balardelle@gmail.com>
2024-05-17 16:42:21 -0400

    sort of works

--- decoder2.py
+++ decoder2.py
@@ -17,14 +17,16 @@ def int_to_char(num: int) -> str:
     char = chr(int(binary[1:], 2))
     return char if char != "\x00" else ""
 
-def ints_to_str(int_list: list, spacing: int) -> str:
+def ints_to_str(int_list: list, spacing: int, end: int) -> str:
     contents = ""
-    for i in range(0, len(int_list), spacing):
+    pixel_processed = 0
+    for i in range(0, len(int_list)):
         for j in range(0, len(int_list[i]), spacing):
+            pixel_processed += 1
             for k in int_list[i][j]:
                 contents += int_to_char(k)
-    
-    return contents
+                if pixel_processed > end:
+                    return contents
 
 with Image.open("outputs/encoded_picture.png") as img:
     img_data = np.array(img)
@@ -41,10 +43,12 @@ with Image.open("outputs/encoded_picture.png") as img:
     print("Width x height:", width, height)
     print("Spacing:", spacing)
 
-    raw_data = ints_to_str(img_data, spacing)
-    ext = raw_data[len(raw_data)-ext_len-1:]
+    raw_data = ints_to_str(img_data, spacing, payload_pixels)
+    end_of_payload = len(raw_data) - ext_len
+    ext = raw_data[end_of_payload + 1 : end_of_payload + ext_len]
+    print("Extention:", ext)
     message = raw_data[:len(raw_data)-ext_len-1]
 
 
-with open("outputs/output.txt", "w") as file:
+with open("outputs/output." + ext, "w") as file:
     file.write(message)
