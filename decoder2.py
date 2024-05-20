9b57fae77cc69e1c4a8fd5230bcbb6dd4bded29e
Balardelle <ibalarde@it37291.bju.edu>
2024-05-20 17:01:25 -0400

    added progress bar to decoder

--- decoder2.py
+++ decoder2.py
@@ -2,6 +2,7 @@
 
 import sys
 import math
+from progress import Progress
 import numpy as np
 from PIL import Image
 from pathlib import Path
@@ -20,14 +21,19 @@ def int_to_char(num: int) -> str:
 def ints_to_str(pixel_list: list, spacing: int, end: int) -> str:
     """Convert a list of RGB values into a string"""
     contents = ""
-    pixel_processed = 0
+    pixels_processed = 0
+    prog = Progress(end, "Decoding file. . .", True, True)
+
     for i in range(0, len(pixel_list)):
         for j in range(0, len(pixel_list[i]), spacing):
-            pixel_processed += 1
+            prog.printProgress(pixels_processed)
+            pixels_processed += 1
             for k in pixel_list[i][j]:
                 contents += int_to_char(k)
-                if pixel_processed > end:
+                if pixels_processed > end:
+                    prog.printComplete("Decoded file")
                     return contents
+
 
 if len(sys.argv) == 2:
     picture_file = sys.argv[1]
@@ -40,26 +46,19 @@ else:
 with Image.open(picture_file) as img:
     img_data = np.array(img)
     payload_length = get_length(img_data[-1][-1]) # last tuple stores length of the payload
-    print("Payload length:", payload_length)
     ext_len = get_length(img_data[-1][-2]) # second-last tuple stores length of the extension
-    print("Extension length:", ext_len)
-    print("Total length:", payload_length + ext_len)
     
     width, height = img.size
     total_pixels = width * (height - 1)
     payload_pixels = (payload_length + ext_len) // 3
     spacing = math.floor(total_pixels // payload_pixels)
-    print("Width x height:", width, height)
-    print("Spacing:", spacing)
+
 
     raw_data = ints_to_str(img_data, spacing, payload_pixels)
     ext = "." + raw_data[:ext_len]
     if not ext[1:].isalnum():
-        print("Invalid extension:", ext)
         ext = ""
     message = raw_data[ext_len:]
-    print("Extension:", ext)
-
 
 with open("outputs/output" + ext, "w") as file:
     file.write(message)
