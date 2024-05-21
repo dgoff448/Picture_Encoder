0764e7ba9129025a46ba8517471ee2b8fbe798cc
Isaac Balardelle <isaac.balardelle@gmail.com>
2024-05-21 08:27:46 -0400

    added fix for colors in Windows CMD + fix for Linux terminals

--- progress.py
+++ progress.py
@@ -1,3 +1,6 @@
+import sys
+from colorama import just_fix_windows_console
+
 class Progress:
     def __init__(self, total:int, barMsg="", showFraction=False, showPercentage=False, barlength:int=20):
         self.emptyChar = "-"
@@ -7,6 +10,7 @@ class Progress:
         self.showFraction = showFraction
         self.showPercentage = showPercentage
         self.barLength = barlength
+        just_fix_windows_console()
 
     def printProgress(self, current:int):
         fullCharAmt = int((self.barLength*current)//self.total)
@@ -14,12 +18,16 @@ class Progress:
         fraction = (f"{current} / {self.total}") if self.showFraction else ("")
         percentage =  (f"{current / self.total:.2%}") if self.showPercentage else ("")
         print(f"\033[1;33m{self.barMsg}\033[00m [\033[1;32m{''.join([self.fullChar for _ in range(fullCharAmt)])}\033[00m{''.join([self.emptyChar for _ in range(emptyCharAmt)])}]   {fraction}   {percentage}", end='\r')
+        sys.stdout.flush()
 
     def printComplete(self, msg:str):
         print(f"\033[1;32m{msg}\033[00m                                                                          ")
+        sys.stdout.flush()
 
     def printInProgress(self, msg:str):
         print(f"\033[1;33m{msg}\033[00m                                                                          ", end='\r')
+        sys.stdout.flush()
 
     def printError(self, msg:str):
         print(f"\033[1;31m{msg}\033[00m                                                                          ")
+        sys.stdout.flush()
